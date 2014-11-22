42f64b817024001ba30b1ce7488ba3b8a76fe5d0
Connor Van Gilder <cvangilder64@gmail.com>
2014-11-21 19:07:27 -0700

    front end

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+   before_filter :authenticate_user!
+#    after_action :verify_authorized
+
+    respond_to :html, :json
+
+    def index
+        @users = User.all
+        respond_with @users
+    end
+
+    def show
+        @user = User.find(params[:id])
+        respond_with @user
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+end
