2ac05c59448864042d2a7db04166bc90cf7888a5
Connor Van Gilder <cvangilder64@gmail.com>
2014-11-21 23:32:04 -0700

    front end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,11 @@ class UsersController < ApplicationController
 
     def index
         @users = User.all
-        def index
   if params[:search]
     @users = User.search(params[:search]).order("created_at DESC")
   else
     @users = User.all.order('created_at DESC')
   end
-end
         respond_with @users
 
     end
